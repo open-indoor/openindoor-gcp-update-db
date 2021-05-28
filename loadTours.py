e199887506776c2efa542fb073edeefa18f7f465
Basile NONCLERCQ <basile@packer-output-1ffe74aa-3fcd-4dd1-84ed-6f3da813b13d>
2021-05-28 10:56:43 +0000

    fix: directory gejson tours file

--- loadTours.py
+++ loadTours.py
@@ -13,7 +13,7 @@ db_host = os.environ["DB_HOST"]
 #Load Tours' data in the postgis database
 def main():
         process = subprocess.run(["ogr2ogr","-f","GeoJSON", "/places-finder/tours.geojson", "/places-finder/tours.osm.pbf", "multipolygons" ])
-        processDB = subprocess.run(["ogr2ogr","-f","PostgreSQL", "PG:dbname="+db_name +" host="+db_host +" port=" +db_port+ " user="+db_user +" password="+db_pass, "tours.geojson", "-nln", "tours" , "-overwrite", "-append", "-update", "-nlt", "MULTIPOLYGON"], capture_output=True)
+        processDB = subprocess.run(["ogr2ogr","-f","PostgreSQL", "PG:dbname="+db_name +" host="+db_host +" port=" +db_port+ " user="+db_user +" password="+db_pass, "/places-finder/tours.geojson", "-nln", "tours" , "-overwrite", "-append", "-update", "-nlt", "MULTIPOLYGON"], capture_output=True)
 
 
 if __name__ == "__main__":
